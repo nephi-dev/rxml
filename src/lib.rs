c05aea8ece6eb0f39ae7f86bf8d67a3b74c45477
Matheus Mendes <mattheus2015@yahoo.com.br>
2023-05-01 20:11:06 -0300

    feat: add write functions

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 use pyo3::prelude::*;
-use quick_xml::events::Event;
-use quick_xml::Reader;
+use quick_xml::events::{BytesEnd, BytesStart, BytesText, Event};
+use quick_xml::{Reader, Writer};
 use std::collections::HashMap;
 use std::fs::File;
 use std::io::Read;
+use std::io::{Cursor, Write};
 
 macro_rules! f_str {
     ($e:expr) => {
@@ -110,6 +111,37 @@ fn read_node(root_tag: String, reader: &mut Reader<&[u8]>) -> Node {
     root
 }
 
+fn write_node(writer: &mut Writer<Cursor<Vec<u8>>>, node: Node) {
+    let mut start = BytesStart::new(&node.name);
+    for (k, v) in node.attrs {
+        start.push_attribute((k.as_str(), v.as_str()));
+    }
+    writer.write_event(Event::Start(start)).unwrap();
+    if let Some(text) = node.text {
+        writer
+            .write_event(Event::Text(BytesText::new(&text)))
+            .unwrap();
+    }
+    for child in node.children {
+        write_node(writer, child);
+    }
+    writer
+        .write_event(Event::End(BytesEnd::new(node.name)))
+        .unwrap();
+}
+
+fn write_node_to_string(node: Node, indent: usize, default_xml_def: bool) -> String {
+    let mut writer = Writer::new_with_indent(Cursor::new(Vec::new()), b' ', indent);
+    write_node(&mut writer, node);
+    let result = writer.into_inner().into_inner();
+    let mut return_string = String::new();
+    if default_xml_def {
+        return_string.push_str("<?xml version=\"1.0\" encoding=\"utf-8\"?>\n");
+    }
+    return_string.push_str(&f_utf!(result));
+    return_string
+}
+
 #[pyfunction]
 fn read_file(file_path: String, root_tag: String) -> Node {
     let mut file = File::open(file_path).unwrap();
@@ -127,10 +159,28 @@ fn read_string(xml_string: String, root_tag: String) -> Node {
     read_node(f_str!(root_tag), &mut reader)
 }
 
+#[pyfunction]
+fn write_file(node: Node, file_path: String, indent: Option<usize>, default_xml_def: Option<bool>) {
+    let _indent = indent.unwrap_or(4);
+    let _default_xml_def = default_xml_def.unwrap_or(true);
+    let mut file = File::create(file_path).unwrap();
+    let xml_string = write_node_to_string(node, _indent, _default_xml_def);
+    file.write_all(xml_string.as_bytes()).unwrap();
+}
+
+#[pyfunction]
+fn write_string(node: Node, indent: Option<usize>, default_xml_def: Option<bool>) -> String {
+    let _indent = indent.unwrap_or(4);
+    let _default_xml_def = default_xml_def.unwrap_or(true);
+    write_node_to_string(node, _indent, _default_xml_def)
+}
+
 #[pymodule]
 fn rxml(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_class::<Node>()?;
     m.add_function(wrap_pyfunction!(read_file, m)?)?;
     m.add_function(wrap_pyfunction!(read_string, m)?)?;
+    m.add_function(wrap_pyfunction!(write_file, m)?)?;
+    m.add_function(wrap_pyfunction!(write_string, m)?)?;
     Ok(())
 }
